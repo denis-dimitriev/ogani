9e702ddd4f61554c2fad1dc3fc96b54f9af7c0d8
WWW <dimitrirv.denis1988@gmail.com>
2023-12-25 12:16:24 +0200

    finished with quick-view prototype

--- client/src/features/ui/product-quick-view.tsx
+++ client/src/features/ui/product-quick-view.tsx
@@ -2,26 +2,44 @@ import { ChangeEvent, useContext, useState } from "react";
 import { QuickViewContext } from "@context/quick-view.context.ts";
 import { ArrowIco, CloseIco } from "@app/assets/icons";
 import { isEmpty } from "@shared/helpers/common.helper.ts";
+import { LanguageContext } from "@context/language.context.tsx";
 
 function ProductQuickView() {
   const { product, setView } = useContext(QuickViewContext);
-
   const [qty, setQTY] = useState(0.5);
+  const { t } = useContext(LanguageContext);
 
   function onInputHandler(e: ChangeEvent<HTMLInputElement>) {
-    if (!isEmpty(e.target.value)) {
-      const qty = parseInt(e.target.value);
-      setQTY(qty);
-    } else {
+    if (isEmpty(e.target.value)) {
       setQTY(0);
+    } else if (isNaN(+e.target.value)) {
+      setQTY(0);
+    } else if (+e.target.value >= 100) {
+      setQTY(100);
+    } else {
+      setQTY(+e.target.value);
     }
   }
+
+  const onIncHandler = () =>
+    setQTY((prev) => {
+      if (prev >= 100) {
+        return 100;
+      }
+      return prev + 0.5;
+    });
+
+  const onDecHandler = () => setQTY((prev) => prev - 0.5);
 
   const onViewExitHandler = () => setView(false);
 
   return (
     <div className="quick-view flex items-center justify-center">
-      <div className="relative h-auto w-[900px] rounded-sm bg-white p-[30px] shadow-2xl">
+      <div
+        className="relative h-[500px] w-[900px]
+       rounded-sm bg-white p-[30px] shadow-2xl tablet:w-[90%]
+       "
+      >
         <button
           className="group absolute right-3 top-2 rounded-full border
          border-[--gray-lighter] p-0.5 transition hover:border-[--gray]"
@@ -31,7 +49,11 @@ function ProductQuickView() {
         </button>
         <div className="relative flex gap-x-5">
           <figure className="flex w-1/2 flex-col">
-            <img src={product?.image || product?.thumbnail} alt="" />
+            <img
+              className=""
+              src={product?.image || product?.thumbnail}
+              alt=""
+            />
           </figure>
           <div className="flex w-1/2 flex-col">
             <h4 className="mb-5">{product?.title}</h4>
@@ -42,22 +64,28 @@ function ProductQuickView() {
             <p className="mb-5">{product?.description}</p>
 
             <div className="flex gap-x-3">
-              <div className="flex gap-x-2">
-                <button className="rotate-90 scale-150">
-                  <ArrowIco />
+              <div className="relative flex gap-x-2">
+                <button disabled={qty <= 0} onClick={onDecHandler}>
+                  <ArrowIco className="rotate-90 scale-150" />
                 </button>
                 <input
-                  type="number"
-                  className="w-[70px] border border-[--gray] pl-4 shadow"
+                  type="text"
+                  inputMode="decimal"
+                  autoFocus
+                  className="w-[45px] border border-[--gray] pl-1
+                  text-center text-[18px] text-[--gray] shadow"
                   value={`${qty}`}
                   onInput={onInputHandler}
                 />
-                <button className="-rotate-90 scale-150">
-                  <ArrowIco />
+                <button onClick={onIncHandler}>
+                  <ArrowIco className="-rotate-90 scale-150" />
                 </button>
               </div>
-              <button className="rounded-md bg-[--dark] px-4 py-2 text-white shadow">
-                Add to cart
+              <button
+                className="rounded-md bg-[--dark] px-4 py-2 text-white transition
+                      hover:bg-[--gray-dark] hover:shadow-xl"
+              >
+                {t?.tips["add to cart"]}
               </button>
             </div>
           </div>
