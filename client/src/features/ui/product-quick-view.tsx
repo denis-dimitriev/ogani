f35995c05001b2422651f74c7e16eb491a2c5476
Denis1988-hub <cryptomining1988@gmail.com>
2024-03-09 16:23:33 +0200

    added skeleton in quick view component

--- client/src/features/ui/product-quick-view.tsx
+++ client/src/features/ui/product-quick-view.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import { QuickViewContext } from "@context/quick-view.context.ts";
 import { ArrowIco, CloseIco } from "@app/assets/icons";
 import { LanguageContext } from "@context/language.context.ts";
@@ -6,17 +6,19 @@ import ApiService from "@app/service/api.service";
 import { ProductType } from "@shared/types/product.types";
 import ShoppingCartStore from "@app/store/shopping-cart.store.ts";
 import { observer } from "mobx-react-lite";
-import Backdrop from "@shared/ui/backdrop.tsx";
-import Spinner from "@shared/ui/spinner.tsx";
+import { ImagesPreloader } from "@shared/helpers/images.preloader.ts";
+import Skeleton from "react-loading-skeleton";
 
 const ProductQuickView = observer(function () {
   const { productID, setView } = useContext(QuickViewContext);
 
   const { t, language } = useContext(LanguageContext);
   const [product, setProduct] = useState<ProductType | never>();
+  const [loading, setLoading] = useState(false);
   const [qty, setQTY] = useState(0);
 
   useEffect(() => {
+    setLoading(true);
     ApiService.getSingleProduct(productID)
       .then((res) => {
         if (res.data) {
@@ -40,6 +42,16 @@ const ProductQuickView = observer(function () {
     }
   }, [product?._id, unitQty]);
 
+  useEffect(() => {
+    if (product) {
+      ImagesPreloader(product.images).finally(() =>
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000),
+      );
+    }
+  }, [product]);
+
   const onIncHandler = () =>
     setQTY((prev) => {
       if (product && prev >= product?.stock) {
@@ -57,14 +69,6 @@ const ProductQuickView = observer(function () {
 
   const onViewExitHandler = () => setView(false);
 
-  if (!product) {
-    return (
-      <Backdrop>
-        <Spinner />
-      </Backdrop>
-    );
-  }
-
   return (
     <div className="backdrop-dark flex items-center justify-center">
       <div
@@ -78,108 +82,136 @@ const ProductQuickView = observer(function () {
         >
           <CloseIco className="scale-75" />
         </button>
-        <div className="grid grid-cols-2 gap-[24px]">
-          <figure className="flex max-h-[450px] flex-col justify-center">
-            <img
-              className="h-full w-full object-contain"
-              src={product.images[0]}
-              alt=""
-            />
-          </figure>
-          <div className="flex flex-col">
-            <h4 className="mb-5">
-              {language === "ro" ? product.name.ro : product.name.ru}
-            </h4>
-            <div className="flex items-center gap-x-2">
-              {product?.promo?.has ? (
-                <>
-                  <p className=" text-[21px]">
-                    {product?.promo.price}
-                    <span className="text-sm">mdl</span>
-                  </p>
-                  <p className="text-[17px] line-through">
-                    {product?.promo.oldPrice}
-                    <span className="text-sm">mdl</span>
-                  </p>
-                </>
-              ) : (
-                <p className=" text-[21px]">
-                  {product?.price}
-                  <span className="text-sm">mdl</span>
-                </p>
-              )}
-            </div>
 
-            {product && product.stock > 0 && (
-              <p className="text-[12px]">{t?.product["in stock"]}</p>
-            )}
-
-            <ul className="mb-5 mt-4 flex flex-col gap-y-[20px] text-sm">
-              <p className="text-[16px] font-bold">{t?.product.info}</p>
-
-              <li className="flex justify-between">
-                <span className="font-semibold">{t?.product.description}</span>
-                <span>
-                  {language === "ro"
-                    ? product?.info.description.ro
-                    : product?.info.description.ru}
-                </span>
-              </li>
-
-              <li className="flex justify-between">
-                <span className="font-semibold">{t?.product.terms}</span>
-                <span>
-                  {language === "ro"
-                    ? product?.info.terms.ro
-                    : product?.info.terms.ru}
-                </span>
-              </li>
-
-              <li className="flex justify-between">
-                <span className="font-semibold">{t?.product.manufacturer}</span>
-                <span>
-                  {language === "ro"
-                    ? product?.info.manufacturer.ro
-                    : product?.info.manufacturer.ru}
-                </span>
-              </li>
-
-              <li className="flex justify-between">
-                <span className="font-semibold">{t?.product.unit}</span>
-                <span>{t?.product[product?.unit as never]}</span>
-              </li>
-            </ul>
-
-            <div className="flex gap-x-3">
-              <div className="relative flex gap-x-2">
-                <button disabled={qty <= 0} onClick={onDecHandler}>
-                  <ArrowIco className="rotate-90 scale-150" />
-                </button>
-                <input
-                  type="text"
-                  inputMode="decimal"
-                  readOnly
-                  className="w-[75px] border border-[--gray] pl-1
-                  text-center text-[18px] text-[--gray] shadow"
-                  value={`${qty}${t?.product[product?.unit as never]}`}
-                />
-                <button
-                  disabled={qty >= 10 || qty >= product?.stock}
-                  onClick={onIncHandler}
-                >
-                  <ArrowIco className="-rotate-90 scale-150" />
-                </button>
+        {!product || loading ? (
+          <div className="grid h-full grid-cols-2 gap-[24px]">
+            <div>
+              <div>
+                <Skeleton className="h-[400px] w-full" />
               </div>
-              <button
-                className="rounded-md bg-[--dark] px-4 py-2 text-white transition
-                      hover:bg-[--gray-dark] hover:shadow-xl"
-                onClick={onAddToCartHandler}
-              >
-                {t?.tips["add to cart"]}
-              </button>
+            </div>
+            <div className="flex flex-col">
+              <Skeleton className="max-w-[200px] p-4" />
+              <Skeleton className="mt-[20px] max-w-[150px] p-2" />
+              <Skeleton className="mt-[20px] max-w-[250px]" />
+              <Skeleton className="mt-[20px] p-5" />
+              <Skeleton className="mt-[20px]" count={3} />
+              <div className="flex justify-start gap-[24px]">
+                <Skeleton className="mt-[20px] min-w-[150px] p-4" />
+                <Skeleton className="mt-[20px] min-w-[150px] p-4" />
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <Fragment>
+            <div className="grid grid-cols-2 gap-[24px]">
+              <figure className="flex max-h-[450px] flex-col justify-center">
+                <img
+                  className="h-full w-full object-contain"
+                  src={product?.images[0]}
+                  alt=""
+                />
+              </figure>
+              <div className="flex flex-col">
+                <h4 className="mb-5">
+                  {language === "ro" ? product?.name.ro : product?.name.ru}
+                </h4>
+                <div className="flex items-center gap-x-2">
+                  {product?.promo?.has ? (
+                    <>
+                      <p className=" text-[21px]">
+                        {product?.promo.price}
+                        <span className="text-sm">mdl</span>
+                      </p>
+                      <p className="text-[17px] line-through">
+                        {product?.promo.oldPrice}
+                        <span className="text-sm">mdl</span>
+                      </p>
+                    </>
+                  ) : (
+                    <p className=" text-[21px]">
+                      {product?.price}
+                      <span className="text-sm">mdl</span>
+                    </p>
+                  )}
+                </div>
+
+                {product && product.stock > 0 && (
+                  <p className="text-[12px]">{t?.product["in stock"]}</p>
+                )}
+
+                <ul className="mb-5 mt-4 flex flex-col gap-y-[20px] text-sm">
+                  <p className="text-[16px] font-bold">{t?.product.info}</p>
+
+                  <li className="flex justify-between">
+                    <span className="font-semibold">
+                      {t?.product.description}
+                    </span>
+                    <span className="ml-16">
+                      {language === "ro"
+                        ? product?.info.description.ro
+                        : product?.info.description.ru}
+                    </span>
+                  </li>
+
+                  <li className="flex justify-between">
+                    <span className="font-semibold">{t?.product.terms}</span>
+                    <span>
+                      {language === "ro"
+                        ? product?.info.terms.ro
+                        : product?.info.terms.ru}
+                    </span>
+                  </li>
+
+                  <li className="flex justify-between">
+                    <span className="font-semibold">
+                      {t?.product.manufacturer}
+                    </span>
+                    <span>
+                      {language === "ro"
+                        ? product?.info.manufacturer.ro
+                        : product?.info.manufacturer.ru}
+                    </span>
+                  </li>
+
+                  <li className="flex justify-between">
+                    <span className="font-semibold">{t?.product.unit}</span>
+                    <span>{t?.product[product?.unit as never]}</span>
+                  </li>
+                </ul>
+
+                <div className="flex gap-x-3">
+                  <div className="relative flex gap-x-2">
+                    <button disabled={qty <= 0} onClick={onDecHandler}>
+                      <ArrowIco className="rotate-90 scale-150" />
+                    </button>
+                    <input
+                      type="text"
+                      inputMode="decimal"
+                      readOnly
+                      className="w-[75px] border border-[--gray] pl-1
+                  text-center text-[18px] text-[--gray] shadow"
+                      value={`${qty}${t?.product[product?.unit as never]}`}
+                    />
+                    <button
+                      disabled={qty >= 10 || qty >= product?.stock}
+                      onClick={onIncHandler}
+                    >
+                      <ArrowIco className="-rotate-90 scale-150" />
+                    </button>
+                  </div>
+                  <button
+                    className="rounded-md bg-[--dark] px-4 py-2 text-white transition
+                      hover:bg-[--gray-dark] hover:shadow-xl"
+                    onClick={onAddToCartHandler}
+                  >
+                    {t?.tips["add to cart"]}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Fragment>
+        )}
       </div>
     </div>
   );
